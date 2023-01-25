50a86d20289f7ccd0b4ac975f35d938ab2e71365
MSI\birdeggb2777 <birdeggb2777@gmail.com>
2023-01-25 19:25:32 +0800

    fix:Incomplete string escaping or encoding.

--- bluelight/scripts/onload.js
+++ bluelight/scripts/onload.js
@@ -243,7 +243,7 @@ function loadLdcmview() {
 
 
 function getParameterByName(name) {
-  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+  name = name.replace(/[\[]/g, "\\[").replace(/[\]]/g, "\\]");
   var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
     results = regex.exec(location.search);
   if (results == null) {
