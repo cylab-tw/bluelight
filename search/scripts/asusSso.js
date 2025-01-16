0abd1cd969c602e70cc519f13895d1fbde15f5f9
CHIN\a5566 <a5566qq2581@gmail.com>
2025-01-16 19:20:22 +0800

    feat(plugin): add asus web storage SSO plugin
    - This is a plugin for commercial usage
    - When enabled, it will redirect to the Raccoon Asus Web Storage page for login
    - After logging in, back to asusSsoCallback.html and save token in localStorage, finally redirect back to the original page

---
+++ search/scripts/asusSso.js
@@ -0,0 +1,51 @@
+
+(async function () {
+    let config = await loadConfig("../data/configAsusSso.json");
+
+    if (config.enabled) {
+        let loginUrl = config.loginUrl;
+        let redirectUrl = config.redirectUrl;
+        const raccoonLoginUrl = `${loginUrl}?redirectUrl=${encodeURIComponent(redirectUrl)}`;
+
+        let token = localStorage.getItem("asusWebStorageToken");
+        if (!token) {
+            localStorage.setItem("bluelightUrl", window.location.href);
+            window.location.href = raccoonLoginUrl;
+        }
+
+        if (config.tokenInRequest) {
+            XMLHttpRequest.prototype.origOpen = XMLHttpRequest.prototype.open;
+
+            XMLHttpRequest.prototype.open = function () {
+                this.origOpen.apply(this, arguments);
+                let decodedToken = JSON.parse(atob(token.split(".")[1]));
+                let expiresAt = decodedToken.exp;
+                let currentTime = Math.floor(Date.now() / 1000);
+
+                if (expiresAt < currentTime) {
+                    window.location.href = raccoonLoginUrl;
+                }
+                this.setRequestHeader('Authorization', "Bearer " + token);
+            };
+            loadLdcmview();
+        }
+    }
+
+    async function loadConfig(url) {
+        return new Promise((resolve, reject) => {
+            let config = {};
+            let requestURL = url;
+            let request = new XMLHttpRequest();
+            request.open('GET', requestURL);
+            request.responseType = 'json';
+            request.send();
+
+            request.onload = function () {
+                config = request.response;
+                return resolve(config);
+            }
+        });
+    }
+})();
+
+
